b2796346e4ddcba9c2ba05b189ca914a4d94cb2a
Михаил Исаков <morgin3.0@yandex.ru>
2023-03-15 16:52:47 +0300

    working with flask 1

--- opinions_app.py
+++ opinions_app.py
@@ -1,11 +1,51 @@
-from flask import Flask
+# what_to_watch/opinions_app.py
+
+from datetime import datetime
+from random import randrange
+
+# Добавлена функция render_template
+from flask import Flask, render_template  
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class Opinion(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), nullable=False)
+    text = db.Column(db.Text, unique=True, nullable=False)
+    source = db.Column(db.String(256))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
 
 @app.route('/')
 def index_view():
-    return 'Совсем скоро тут будет случайное мнение о фильме!'
+    quantity = Opinion.query.count()
+    if not quantity:
+        return 'В базе данных записей нет.'
+    offset_value = randrange(quantity)
+    opinion = Opinion.query.offset(offset_value).first()
+    # Тут подключается новый шаблон
+    return render_template('opinion.html', opinion=opinion)
+    
+@app.route('/add')
+def add_opinion_view():
+    # И тут тоже
+    return render_template('add_opinion.html')
+
+# Тут указывается конвертер пути для id
+@app.route('/opinions/<int:id>')  
+
+def opinion_view(id):  
+    # Метод get заменён на метод get_or_404()
+    opinion = Opinion.query.get_or_404(id)  
+    return render_template('opinion.html', opinion=opinion)  
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run() 
