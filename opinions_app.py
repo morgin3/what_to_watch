68313999670a43ec3d8cf2e6af7ea851d0534ad2
Михаил Исаков <morgin3.0@yandex.ru>
2023-03-16 12:49:21 +0300

    errors templates

--- opinions_app.py
+++ opinions_app.py
@@ -3,14 +3,18 @@
 from datetime import datetime
 from random import randrange
 
-# Добавлена функция render_template
-from flask import Flask, render_template  
+from flask import abort
+from flask import Flask, redirect, render_template, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField, URLField
+from wtforms.validators import DataRequired, Length, Optional
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'MY SECRET KEY'
 
 db = SQLAlchemy(app)
 
@@ -23,29 +27,72 @@ class Opinion(db.Model):
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
 
+class OpinionForm(FlaskForm):
+    title = StringField(
+        'Введите название фильма',
+        validators=[DataRequired(message='Обязательное поле'),
+                    Length(1, 128)]
+    )
+    text = TextAreaField(
+        'Напишите мнение', 
+        validators=[DataRequired(message='Обязательное поле')]
+    )
+    source = URLField(
+        'Добавьте ссылку на подробный обзор фильма',
+        validators=[Length(1, 256), Optional()]
+    )
+    submit = SubmitField('Добавить')
+
+
 @app.route('/')
 def index_view():
     quantity = Opinion.query.count()
     if not quantity:
-        return 'В базе данных записей нет.'
+        abort(404)
     offset_value = randrange(quantity)
     opinion = Opinion.query.offset(offset_value).first()
-    # Тут подключается новый шаблон
-    return render_template('opinion.html', opinion=opinion)
+    return render_template('opinion.html', opinion=opinion) 
     
-@app.route('/add')
+
+@app.route('/add', methods=['GET', 'POST'])
 def add_opinion_view():
-    # И тут тоже
-    return render_template('add_opinion.html')
+    form = OpinionForm()
+    if form.validate_on_submit():
+        text = form.text.data
+        # Если в БД уже есть мнение с текстом, который ввёл пользователь,
+        if Opinion.query.filter_by(text=text).first() is not None:
+            # вызвать функцию flash и передать соответствующее сообщение
+            flash('Такое мнение уже было оставлено ранее!')
+            # и вернуть пользователя на страницу «Добавить новое мнение»
+            return render_template('add_opinion.html', form=form)
+        opinion = Opinion(
+            title=form.title.data, 
+            text=form.text.data, 
+            source=form.source.data
+        )
+        db.session.add(opinion)
+        db.session.commit()
+        return redirect(url_for('opinion_view', id=opinion.id))
+    return render_template('add_opinion.html', form=form)
 
-# Тут указывается конвертер пути для id
+
 @app.route('/opinions/<int:id>')  
-
 def opinion_view(id):  
-    # Метод get заменён на метод get_or_404()
     opinion = Opinion.query.get_or_404(id)  
-    return render_template('opinion.html', opinion=opinion)  
+    return render_template('opinion.html', opinion=opinion)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    # В качестве ответа возвращается собственный шаблон 
+    # и код ошибки
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    # В таких случаях можно откатить незафиксированные изменения в БД
+    db.session.rollback()
+    return render_template('500.html'), 500
 
 
 if __name__ == '__main__':
-    app.run() 
+    app.run()
